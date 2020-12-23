a7c5257921788ab9900fbb4bf354c544593cdaee
kytoaq <kytoaq@gmail.com>
2020-12-23 04:48:39 +0000

    doha: Init device tree
    Signed-off-by: kytoaq <kytoaq@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2020 - 2021 Doritos™ Motorola | Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/motorola/doha
+BOARD_VENDOR := motorola
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2020-08-01
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.atrace@1.0-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.cas@1.1-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.gnss@2.0-service-qti.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vendor.qti.gnss@3.0-service.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/motorola/doha/BoardConfigVendor.mk
---
+++ idc/uinput-fpc.idc
@@ -0,0 +1,15 @@
+#
+# FPC1020 Touch Sensor Driver
+#
+# Copyright (C) 2013 - 2014 Fingerprint Cards AB <tech@fingerprints.com>
+# Copyright (C) 2020 - 2021 Doritos™ Motorola | Developers
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License Version 2
+# as published by the Free Software Foundation.
+#
+
+device.internal = 1
+keyboard.layout = uinput-fpc
+keyboard.builtIn = 1
+keyboard.orientationAware = 1
